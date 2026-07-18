a693622f3b5753e92bbe65a88b5b73577a88eb9d
Manali-thakur <manalithakur7062@gmail.com>
2026-07-18 16:53:08 +0530

    middleware validation

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,37 +1,32 @@
 // Hoisted Declaration => a function
 // class
 // assignment expression
+import { body, validationResult } from "express-validator";
 
-const validateRequest = (req, res, next) => {
+// validationResult for errors
+
+const validateRequest = async (req, res, next) => {
   // validate the data
-  const { title, price, description, category, image, rating, count } =
-    req.body;
 
-  let errors = [];
+  // 1.Setup the rules for validation.
+  const rules = [
+    body("title").isEmpty().withMessage("Name is Required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be a positive value"),
+    body("image").isURL().withMessage("Invalid URL"),
+    body("description").isEmpty().withMessage("Description is Necessary"),
+    body("rating").isFloat({ gt: 0 }).withMessage("Rating should more than 0"),
+    body("count").isFloat({ gt: 0 }).withMessage("Count is Invalid"),
+  ];
 
-  if (!title || title.trim() == "") {
-    errors.push("Name is Required");
-  }
-  if (!price || parseFloat(price) < 1) {
-    errors.push("Price is Invalid");
-  }
-  if (!description || description.trim() == "") {
-    errors.push("Description is Required");
-  }
-  if (description.length > 0 && description.length < 10) {
-    errors.push("Description should be more than 10 characters");
-  }
-  if (!rating || parseFloat(rating) < 1) {
-    errors.push("Rating is Invalid");
-  }
-  if (!count || parseFloat(count) < 1) {
-    errors.push("Count is Invalid");
-  }
-  try {
-    const validUrl = new URL(image);
-  } catch (err) {
-    errors.push("URL is Invalid");
-  }
+  // 2.Run those Rules
+  await Promises.all(rules.map((rule) => rule.run(req)));
+
+  // 3. Check if there are any error after running the rules
+  // will return all the errors
+  var validationErrors = validationResult(req);
+  
 
   if (errors.length > 0) {
     return res.render("new-product", {
@@ -43,6 +38,5 @@ const validateRequest = (req, res, next) => {
   // if no error then it will point to another middleware to the pipeline
   next();
 };
-
 
 export default validateRequest;
